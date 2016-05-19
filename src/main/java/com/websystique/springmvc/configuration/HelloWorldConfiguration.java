cc7dc902efc59d1f0ceb50754085496261dd1eef
Sunil Mane <Sunil Mane>
2016-05-19 08:09:52 +0530

    Demo App

---
+++ src/main/java/com/websystique/springmvc/configuration/HelloWorldConfiguration.java
@@ -0,0 +1,19 @@
+package com.websystique.springmvc.configuration;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.ViewResolverRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
+import org.springframework.web.servlet.view.JstlView;
+
+@Configuration
+@EnableWebMvc
+@ComponentScan(basePackages = "com.websystique.springmvc")
+public class HelloWorldConfiguration{
+	
+
+
+}
